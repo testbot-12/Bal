7cfe4d0a91ca1f8776f495aedfb0cf0e9b6f642c
Pharouk34 <164754483+Pharouk34@users.noreply.github.com>
2024-04-16 20:22:13 +0000

    Update translate.js

--- scripts/cmds/translate.js
+++ scripts/cmds/translate.js
@@ -43,7 +43,7 @@ module.exports = {
 
 		},
 		en: {
-			translateTo: "🌐 Translate from %1 to %2",
+			translateTo: "╔══════════════════╗\n\n 🔬|𝗧𝗿𝗮𝗻𝘀𝗹𝗮𝘁𝗲 𝗳𝗿𝗼𝗺 『%1』 𝘁𝗼 『%2』\n\n╚══════════════════╝",
 			invalidArgument: "❌ Invalid argument, please choose on or off",
 			turnOnTransWhenReaction: `✅ Turn on translate message when reaction, try to react \"${defaultEmojiTranslate}\" to any message to translate it (not support bot message)\n Only translate message after turn on this feature`,
 			turnOffTransWhenReaction: "✅ Turn off translate message when reaction",
